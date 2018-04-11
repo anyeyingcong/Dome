3e84e1127f810cb1b93c5c5d735bf4689363d911
anyeyingcong <449329301@qq.com>
2018-04-11 15:57:27 +0800

    dome
    dome

---
+++ dome/dome/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  dome
+//
+//  Created by 王俊杰 on 2018/4/11.
+//  Copyright © 2018年 刘讲话. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
